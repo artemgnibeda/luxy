d554b0ccc714d1fcccc8e5ed344aecae359ce0e0
artemgnibeda <artemgnibeda1501@gmail.com>
2019-05-26 19:34:59 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(".show-image").click(function() {
+    event.preventDefault();
+    var mainImage = $(this).index();
+    $('.product-photo-section img').removeClass('active');
+    $('.product-photo-section img').eq(mainImage).addClass('active');
+});
+
+$(function () {
+	$('.popup-modal').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+		modal: true
+	});
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+});
+
+$('.slider-popup').slick({
+  infinite: true,
+  dots: false,
+  slidesToShow: 6,
+  slidesToScroll: 3,
+  nextArrow: '<button class="slick-arrow nt slick-next"><i class="fas fa-chevron-right"></i></button> ',
+  prevArrow: '<button class="slick-arrow pr slick-prev"><i class="fas fa-chevron-left"></i></button> '
+});
+
+$(document).ready(function() {
+  $(".accordion .accord-header").click(function() {
+
+      if($(this).next("div").is(":visible")){
+
+        $(this).next("div").slideUp("slow");
+
+      } else {
+        $(".accordion .accord-content").slideUp("slow");
+        $(this).next("div").slideToggle("middle");
+        
+      }
+  });
+});
